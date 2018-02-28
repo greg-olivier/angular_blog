3741da4c5b87edd2e10921e02312c70e1a320cbb
Greg <35299257+greg-olivier@users.noreply.github.com>
2018-02-28 16:45:04 +0100

    add data, reset form, bootstrap, add article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, NgForm} from '@angular/forms';
 import {Article} from './article';
 
 @Component({
@@ -8,6 +8,50 @@ import {Article} from './article';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'app';
+    title: string;
+    articles: Array<Article>;
+    editing: boolean;
+    editArticle: Article;
+    showall: boolean;
+    contact: boolean;
+
+    constructor() {
+        this.editing = false;
+        this.showall = true;
+        this.contact = false;
+        this.editArticle = new Article(0, '');
+        this.title = 'My First Angular Blog';
+        this.articles = new Array();
+        this.articles.push(new Article(99, 'Le monde merveilleux de oui-oui', 'Quisque blandit luctus quam, ut tincidunt lorem pretium eu. Nam enim massa, mollis vitae lacus ut, accumsan sodales lectus. Nullam eu elit quis magna imperdiet consequat in id eros. Duis at tellus non elit fermentum volutpat.'));
+    }
+
+    backToList() {
+        setTimeout(() =>
+            this.showAll());
+    }
+
+    showAll() {
+        this.showall = true;
+        this.editing = false;
+        this.contact = false;
+    }
+
+    addArticle() {
+        this.editing = true;
+        this.showall = false;
+        this.contact = false;
+    }
+
+    showContact() {
+        this.editing = false;
+        this.showall = false;
+        this.contact = true;
+    }
+
+    saveArticle(myForm: NgForm) {
+        console.info(myForm);
+        this.articles.push(JSON.parse(JSON.stringify(this.editArticle)));
+        myForm.resetForm();
+    }
 }
 
